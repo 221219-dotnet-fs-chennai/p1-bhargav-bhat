1284c0d99a851a07ad0eb33fbe841fafac8dead7
bhargavbhat7342 <BhargavBhat742@gmail.com>
2022-12-20 18:17:51 +0530

    Work for today

---
+++ Assignments/Activity1.md
@@ -0,0 +1,6 @@
+# Commands used
+	- mkdir
+	- cd
+	- vi Acitivity1.sh / touch Acitvity.sh
+	- echo -e 
+	- read -p
---
+++ Assignments/Activity1.sh
@@ -0,0 +1,7 @@
+echo "Welcome to Alchemy:)"
+read -p "Enter your First name: " fname
+read -p "Enter your Last name: " lname
+read -p "Enter your date of birth: " DoB
+echo -e "\n Welcome , $fname $lname"
+echo -e "\n DoB : $DoB"
+echo -e "\n Thank you"
